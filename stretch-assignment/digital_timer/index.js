0480859d6d8efe2bd168dd803bfaed66caaad944
agohorel <gohorelalex@gmail.com>
2019-12-02 18:32:23 -0500

    timer stretch #1 - add button to start timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -4,30 +4,34 @@ const secondsOnes = document.querySelector("#secondOnes");
 const msHundreds = document.querySelector("#msHundreds");
 const msTens = document.querySelector("#msTens");
 
-let started = Date.now();
+const button = document.querySelector("button");
 
-let timer = setInterval(function() {
-  let ellapsed = Date.now() - started;
-  if (ellapsed >= 10000) {
-    clearTimeout(timer);
-    [secondsOnes, secondsTens, msHundreds, msTens].forEach(item =>
-      item.classList.add("redDigit")
-    );
-  }
+button.addEventListener("click", function() {
+  let started = Date.now();
 
-  secondsTens.textContent = Math.floor(ellapsed / 1000)
-    .toString()
-    .substring(0, 1);
+  let timer = setInterval(function() {
+    let ellapsed = Date.now() - started;
+    if (ellapsed >= 10000) {
+      clearTimeout(timer);
+      [secondsOnes, secondsTens, msHundreds, msTens].forEach(item =>
+        item.classList.add("redDigit")
+      );
+    }
 
-  secondsOnes.textContent = Math.floor(ellapsed / 100)
-    .toString()
-    .substring(1, 2);
+    secondsTens.textContent = Math.floor(ellapsed / 1000)
+      .toString()
+      .substring(0, 1);
 
-  msHundreds.textContent = Math.floor(ellapsed / 10)
-    .toString()
-    .substring(2, 3);
+    secondsOnes.textContent = Math.floor(ellapsed / 100)
+      .toString()
+      .substring(1, 2);
 
-  msTens.textContent = Math.floor(ellapsed)
-    .toString()
-    .substring(3, 4);
-}, 10);
+    msHundreds.textContent = Math.floor(ellapsed / 10)
+      .toString()
+      .substring(2, 3);
+
+    msTens.textContent = Math.floor(ellapsed)
+      .toString()
+      .substring(3, 4);
+  }, 10);
+});
