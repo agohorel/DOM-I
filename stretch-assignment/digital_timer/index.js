9a9323a8f04287606308c7f91cb730a341dbf38e
agohorel <gohorelalex@gmail.com>
2019-12-02 18:10:29 -0500

    add timer stretch goal

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,33 @@
+const secondsTens = document.querySelector("#secondTens");
+const secondsOnes = document.querySelector("#secondOnes");
+
+const msHundreds = document.querySelector("#msHundreds");
+const msTens = document.querySelector("#msTens");
+
+let started = Date.now();
+
+let timer = setInterval(function() {
+  let ellapsed = Date.now() - started;
+  if (ellapsed >= 10000) {
+    clearTimeout(timer);
+    [secondsOnes, secondsTens, msHundreds, msTens].forEach(item =>
+      item.classList.add("redDigit")
+    );
+  }
+
+  secondsTens.textContent = Math.floor(ellapsed / 1000)
+    .toString()
+    .substring(0, 1);
+
+  secondsOnes.textContent = Math.floor(ellapsed / 100)
+    .toString()
+    .substring(1, 2);
+
+  msHundreds.textContent = Math.floor(ellapsed / 10)
+    .toString()
+    .substring(2, 3);
+
+  msTens.textContent = Math.floor(ellapsed)
+    .toString()
+    .substring(3, 4);
+}, 10);
